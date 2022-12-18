8f73fe9b500eb3a51322b3659cf6e9f14d3ef0c9
Rafida <rafidakn@gmail.com>
2022-12-18 13:45:58 +0100

    pages and navigation setup

--- App.js
+++ App.js
@@ -1,9 +1,31 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import * as eva from '@eva-design/eva';
+import {ApplicationProvider} from '@ui-kitten/components';
+import HomeScreen from './src/screens/HomeScreen';
+import LoginScreen from './src/screens/LoginScreen';
 
- const App = () =>{
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  let isLoggedIn = false;
+
   return (
-    <NavigationContainer></NavigationContainer>
+    <NavigationContainer>
+      <ApplicationProvider {...eva} theme={eva.light}>
+        {isLoggedIn ? (
+          <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen name="HomeScreen" component={HomeScreen} />
+          </Stack.Navigator>
+        ) : (
+          <Stack.Navigator initialRouteName="LoginScreen">
+            <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          </Stack.Navigator>
+        )}
+      </ApplicationProvider>
+    </NavigationContainer>
   );
-} 
-export default App
+};
+
+export default App;
